757a688e6437ad539d55e876113cfaf49edeb9fe
Eugine Park <pkeugine@gmail.com>
2020-05-03 02:10:30 +0900

    c language compile process

---
+++ CFileBuildProcess/HelloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int number = 10;
+    char letter = 'e';
+
+    printf("Hello, with a number %d, and a letter %c.\n", number, letter);
+    return 0;
+}
---
+++ CFileBuildProcess/HelloWorld.s
@@ -0,0 +1,35 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 15	sdk_version 10, 15, 4
+	.globl	_main                   ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	movl	$10, -8(%rbp)
+	movb	$101, -9(%rbp)
+	movl	-8(%rbp), %esi
+	movsbl	-9(%rbp), %edx
+	leaq	L_.str(%rip), %rdi
+	movb	$0, %al
+	callq	_printf
+	xorl	%ecx, %ecx
+	movl	%eax, -16(%rbp)         ## 4-byte Spill
+	movl	%ecx, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"Hello, with a number %d, and a letter %c.\n"
+
+
+.subsections_via_symbols
