2149fcfbb599f350c7e86e93078c79d865500364
Eugine Park <pkeugine@gmail.com>
2020-07-31 17:49:53 +0000

    edit swift

--- Playground/UIKitPractice.playground/Contents.swift
+++ Playground/UIKitPractice.playground/Contents.swift
@@ -7,7 +7,7 @@ class MyViewController : UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
-
+        let thisthing = 1
         let label = UILabel()
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
         label.text = "Hello World!"
