94027522561312fd58b5b941338f09957f01bdfe
Eugine Park <pkeugine@gmail.com>
2020-08-04 23:18:29 +0900

    practice kotlin

--- Kotlin/main.kt
+++ Kotlin/main.kt
@@ -1,3 +1,14 @@
+class Eugine {
+    var age: Int = 25
+        private set;
+};
+
+fun countString(word: String, formula: (String)->Int)): Int {
+    return formula(word);
+}
 fun main(args: Array<String>) {
-    
+    val hihiNum = countString("hihi") {
+        input->input.length;
+    }
+    println(hihiNum);
 }
