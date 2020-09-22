6511bd2b8231a1f945d99a2bff8b4aa68e6edd1b
Eugine Park <pkeugine@gmail.com>
2020-09-22 13:04:03 +0000

    try out cargo

---
+++ Rust/practice/target/debug/deps/practice-11a1d816ae9cbfcf.d
@@ -0,0 +1,5 @@
+/home/runner/multilingual/Rust/practice/target/debug/deps/practice-11a1d816ae9cbfcf: src/main.rs
+
+/home/runner/multilingual/Rust/practice/target/debug/deps/practice-11a1d816ae9cbfcf.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/practice/target/debug/practice.d
@@ -0,0 +1,1 @@
+/home/runner/multilingual/Rust/practice/target/debug/practice: /home/runner/multilingual/Rust/practice/src/main.rs
